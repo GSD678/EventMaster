e6a52e052d1e281da1f0d15910f7bcfe6fd5ae3e
Jheel Goel <mailjheel@gmail.com>
2022-08-29 17:46:57 -0700

    fixed cart screen and event screen

--- frontend/src/Screens/CartScreen.js
+++ frontend/src/Screens/CartScreen.js
@@ -51,15 +51,16 @@ const CartScreen = () => {
                   <Col md={3}>
                     <Link to={`/event/${item.event}`}>{item.name}</Link>
                   </Col>
-                  <Col md={2}>${item.price}</Col>
+                  <Col md={2}>${item.ticketprice}</Col>
                   <Col md={2}>
                     <Form.Control
                       as='select'
                       value={item.qty}
                       onChange={(e) =>
-                        dispatch(
-                          addToCart(item.event, Number(e.target.value))
-                        )
+                        {
+                          e.target.value = e.target.value > 0 ? e.target.value : 1;
+                          dispatch(addToCart(item.event, Number(e.target.value - item.qty)));
+                        }
                       }
                     >
                       {[...Array(item.ticketsavail).keys()].map((x) => (
@@ -94,7 +95,7 @@ const CartScreen = () => {
               </h2>
               $
               {cartItems
-                .reduce((acc, item) => acc + item.qty * item.price, 0)
+                .reduce((acc, item) => acc + item.qty * item.ticketprice, 0)
                 .toFixed(2)}
             </ListGroup.Item>
             <ListGroup.Item>
--- frontend/src/Screens/EventScreen.js
+++ frontend/src/Screens/EventScreen.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import {useDispatch, useSelector} from "react-redux"
 import { Link, useParams, useNavigate } from 'react-router-dom'
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import { Row, Col, Image, ListGroup, Card, Button, Form } from 'react-bootstrap'
 import Event from '../components/Event'
 import {listEventDetails} from "../actions/eventAction.js"
 import Loader from "../components/Loader";
@@ -9,7 +9,7 @@ import Loader from "../components/Loader";
 
 
 const EventScreen = () => {
-   const[qty,setQty] = useState(0);
+   const[qty,setQty] = useState(1);
    const params = useParams();
    const dispatch = useDispatch();
    const navigate = useNavigate();
@@ -21,7 +21,7 @@ const EventScreen = () => {
    }, [dispatch,params.id])
 
 const addToCartHandler = () => {
-    navigate(`/cart/${params.id}?qty={qty}`)
+    navigate(`/cart/${params.id}?qty=${qty}`)
 }
 
     return (
@@ -73,27 +73,26 @@ const addToCartHandler = () => {
                                     </Col>
                                 </Row>
                             </ListGroup.Item>
-                            {event.countInStock > 0 && (
-                             <ListGroup.Item>
-                              <Row>
-                                <col>Qty</col>
-                                <col>
-                                 <Form.Control
-                                  as='select'
-                                  value={qty}
-                                  onChange={(e) => setQty(e.target.value)} 
-                                  >
-                                   {[...Array(event.countInStock).keys()].map((x) => (
-                                       <option key={x + 1} value={x + 1}>
-                                      {x + 1}
-                                     </option>
-                                   ))}
-
-                                  </Form.Control>
-                                 </col>
-                                </Row>
-                               </ListGroup.Item>
-                            )}
+                              {event.ticketsavail > 0 && (
+                                <ListGroup.Item>
+                                  <Row>
+                                    <Col>Qty</Col>
+                                    <Col>
+                                      <Form.Control
+                                        as="select"
+                                        value={qty}
+                                        onChange={(e) => setQty(e.target.value)}
+                                      >
+                                        {[...Array(event.ticketsavail).keys()].map((x) => (
+                                          <option key={x + 1} value={x + 1}>
+                                            {x + 1}
+                                          </option>
+                                        ))}
+                                      </Form.Control>
+                                    </Col>
+                                  </Row>
+                                </ListGroup.Item>
+                              )}
 
 
 
