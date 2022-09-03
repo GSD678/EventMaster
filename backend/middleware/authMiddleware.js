4ee5a3865bce3be56ddca3e7b68594105877bf1c
rudrayy <micky.huangyy@gmail.com>
2022-09-02 23:28:02 -0700

    backend/middleware/authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -7,12 +7,16 @@ const protect = asyncHandler(async(req, res, next)=> {
 
     if (req.headers.authorization && req.headers.authorization.
         startsWith('Bearer')){
+        try{
         token = req.headers.authorization.split(' ')[1]
         const decoded = jwt.verify(token,process.env.JWT_SECRET)  
         req.user = await User.findById(decoded.id).select('-password')
         next()
-
+        } catch (error){
+        res.status(401)
+        throw new Error('Not authorized, invalid token')
         }
+    }
 
     if (!token){
        res.status(401)
