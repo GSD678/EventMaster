2951f7237dcb853c55b10aca596e0a19dd4193c7
rudrayy <micky.huangyy@gmail.com>
2022-09-05 15:31:54 -0700

    add user registration

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -22,6 +22,18 @@ const authUser = asyncHandler(async (req, res) => {
 
 
 const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
 
 })
 
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,18 +1,25 @@
 import {configureStore, combineReducers} from '@reduxjs/toolkit'
 import { eventListReducer, eventDetailsReducer} from './reducers/eventReducer'
 import { cartReducer } from './reducers/cartReducers'
+import { userLoginReducer } from './reducers/userReducers'
 
 const rootReducer = combineReducers({
   eventList: eventListReducer,
   eventDetails: eventDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 })
 const cartItemsFromStorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
   : []
 
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null
+
 const initialState = {
-  cart: {cartItems:  cartItemsFromStorage}
+  cart: {cartItems:  cartItemsFromStorage},
+  userLogin: { userInfo:userInfoFromStorage},
 }
 const store = configureStore({
   reducer: rootReducer,
